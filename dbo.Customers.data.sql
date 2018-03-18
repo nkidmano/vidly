4c3319f55f19711d16f73f360bd48b6274017cab
nkidmano <nobudy55@gmail.com>
2018-03-18 09:41:45 +0700

    Finish Working with Data section, but not doing exercise yet

---
+++ dbo.Customers.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Customers] ON
+INSERT INTO [dbo].[Customers] ([Id], [Name], [IsSubcribedToNewsLetter], [MembershipTypeId]) VALUES (1, N'John Smith', 0, 1)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [IsSubcribedToNewsLetter], [MembershipTypeId]) VALUES (2, N'Marry Williams', 1, 2)
+SET IDENTITY_INSERT [dbo].[Customers] OFF
---
+++ vidly/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public Byte DurationInMonths { get; set; }
+        public short DiscountRate { get; set; }
+    }
+}
