3a01017f319d49c86a2978dda164a320c1ba6846
nkidmano <nobudy55@gmail.com>
2018-03-18 10:10:51 +0700

    Reset auto-increment by using TRUNCATE TABLE Customers

--- dbo.Customers.data.sql
+++
@@ -1,4 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Customers] ON
-INSERT INTO [dbo].[Customers] ([Id], [Name], [IsSubcribedToNewsLetter], [MembershipTypeId]) VALUES (1, N'John Smith', 0, 1)
-INSERT INTO [dbo].[Customers] ([Id], [Name], [IsSubcribedToNewsLetter], [MembershipTypeId]) VALUES (2, N'Marry Williams', 1, 2)
-SET IDENTITY_INSERT [dbo].[Customers] OFF
